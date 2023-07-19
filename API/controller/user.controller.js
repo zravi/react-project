58f5fb135aaded1ae6d534815306fde04863e8d6
RΛVI <zravi@outlook.com>
2023-07-19 12:33:49 +0530

    0
    0

--- API/controller/user.controller.js
+++ API/controller/user.controller.js
@@ -3,44 +3,52 @@ import * as url from 'url';
 import jwt from 'jsonwebtoken';
 import sendMail from './email.controller.js';
 
-//to link schema model
+// schema model
 import UserSchemaModel from '../model/user.model.js';
 
-export var save=async (req,response,next)=>{
-  var userDetails=req.body
-  sendMail(userDetails.email,userDetails.password);
-  var userList = await UserSchemaModel.find();
-  var l=userList.length;
-  var _id=l==0?1:userList[l-1]._id+1;
-  userDetails={...userDetails,"_id":_id,"status":0,"role":"user","info":Date()};
-  var user = await UserSchemaModel.create(userDetails);
-  if(user)
-    return response.status(201).json({"result":"User register successfully...."});
-  else
-    return response.status(500).json({"result": "Server Error"});
-}
-
-export var fetch=async (req,response,next)=>{
-  var condition_obj=url.parse(req.url,true).query;
-  var userList = await UserSchemaModel.find(condition_obj);
-  if(userList.length!=0)
-    return response.status(201).json(userList);
-  else
-    return response.status(500).json(userList);
-}
-
-export var deleteUser=async(request,response,next)=>{
-  var id = request.params.id;
-  var user = await UserSchemaModel.find({_id: id});
-  if(user.length!=0){
-    let result = await UserSchemaModel.deleteMany({_id:id}); 
-    if(result)
-     return response.status(201).json({"msg":"success"});
+export var save = async (req, response, next) => {
+  try {
+    var userDetails = req.body
+    sendMail(userDetails.email, userDetails.password);
+    var userList = await UserSchemaModel.find();
+    var l = userList.length;
+    var _id = l == 0 ? 1 : userList[l - 1]._id + 1;
+    userDetails = { ...userDetails, "_id": _id, "status": 0, "role": "user", "info": Date() };
+    var user = await UserSchemaModel.create(userDetails);
+    if (user)
+      return response.status(201).json({ "result": "User register successfully...." });
     else
-     return response.status(500).json({error: 'Server Error'});
-  }
-  else
-    return response.status(404).json({error: 'Resource not found'});             
+      return response.status(500).json({ "result": "Server Error" });
+  } catch (err) { console.log(err) }
+}
+
+export var fetch = async (req, response, next) => {
+  try {
+    console.log(req.body)
+    var condition_obj = url.parse(req.url, true).query;
+    var userList = await UserSchemaModel.find(condition_obj);
+    if (userList.length != 0)
+      return response.status(201).json(userList);
+    else
+      return response.status(500).json(userList);
+  } catch (err) { console.log(err) }
+}
+
+export var deleteUser = async (request, response, next) => {
+  try {
+    console.log(request.body, request.params)
+    var id = request.params.id;
+    var user = await UserSchemaModel.find({ _id: id });
+    if (user.length != 0) {
+      let result = await UserSchemaModel.deleteMany({ _id: id });
+      if (result)
+        return response.status(201).json({ "msg": "success" });
+      else
+        return response.status(500).json({ error: 'Server Error' });
+    }
+    else
+      return response.status(404).json({ error: 'Resource not found' });
+  } catch (err) { console.log(err) }
 }
 
 // export var updateUser=async(request,response,next)=>{
@@ -59,22 +67,25 @@ export var deleteUser=async(request,response,next)=>{
 
 
 export var updateUser = async (request, response, next) => {
-  const id = request.params.id;
-  const condition = id ? { _id: id } : request.body.condition;
-  const userDetails = await UserSchemaModel.findOne(condition);
+  try {
+    console.log(request.body, request.params)
+    const id = request.params.id;
+    const condition = id ? { _id: id } : request.body.condition;
+    const userDetails = await UserSchemaModel.findOne(condition);
 
-  if (userDetails) {
-    const updateQuery = id ? request.body : { $set: request.body.set };
-    const user = await UserSchemaModel.updateOne(condition, updateQuery);
+    if (userDetails) {
+      const updateQuery = id ? request.body : { $set: request.body.set };
+      const user = await UserSchemaModel.updateOne(condition, updateQuery);
 
-    if (user) {
-      return response.status(201).json({ "msg": "success" });
+      if (user) {
+        return response.status(201).json({ "msg": "success" });
+      } else {
+        return response.status(500).json({ error: "Server Error" });
+      }
     } else {
-      return response.status(500).json({ error: "Server Error" });
+      return response.status(404).json({ error: "Requested resource not available" });
     }
-  } else {
-    return response.status(404).json({ error: "Requested resource not available" });
-  }
+  } catch (err) { console.log(err) }
 };
 
 // export var updateUser=async(request,response,next)=>{
@@ -91,18 +102,20 @@ export var updateUser = async (request, response, next) => {
 //    return response.status(404).json({error: "Requested resource not available"});
 // }
 
-export var login=async (req,response,next)=>{
-  var userDetails=req.body;
-  userDetails={...userDetails,"status":1};
-  var userList = await UserSchemaModel.find(userDetails);
-  if(userList.length!=0)
-  {
-    let payload={"subject":userList[0].email};  
-    let token=jwt.sign(payload,"qwdbqkbdkqwd");
-    return response.status(201).json({"token":token,"userDetails":userList[0]});
-  }
-  else
-    return response.json({"token":"error"});
+export var login = async (req, response, next) => {
+  try {
+    console.log(req.body)
+    var userDetails = req.body;
+    userDetails = { ...userDetails, "status": 1 };
+    var userList = await UserSchemaModel.find(userDetails);
+    if (userList.length != 0) {
+      let payload = { "subject": userList[0].email };
+      let token = jwt.sign(payload, "TOKEN");
+      return response.status(201).json({ "token": token, "userDetails": userList[0] });
+    }
+    else
+      return response.json({ "token": "error" });
+  } catch (err) { console.log(err) }
 }
 
 
--- API/model/connection.js
+++ API/model/connection.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 //var url="mongodb://127.0.0.1:27017/pixx";
-var url="mongodb+srv://ravi:uQS6eh7eYRpdUaXu@cluster0.fhq6hee.mongodb.net/Cluster0?retryWrites=true&w=majority";
+var url = "mongodb+srv://ravi:uQS6eh7eYRpdUaXu@cluster0.fhq6hee.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 mongoose.set('strictQuery', true);
-mongoose.connect(url);
-console.log("Successfully connected to mongodb database");
+try {
+    mongoose.connect(url);
+    console.log("Successfully connected to mongodb database");
+} catch (err) { console.log(err) }
